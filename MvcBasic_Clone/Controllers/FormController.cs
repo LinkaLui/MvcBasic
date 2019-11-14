dbea61a2f6f7a46fe3229dc1066e14a521fc6098
Linka <sania0321@gmail.com>
2019-11-14 17:02:41 +0800

    加入專案檔案。

---
+++ MvcBasic_Clone/Controllers/FormController.cs
@@ -0,0 +1,60 @@
+﻿using MvcBasic_Clone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FormContext = MvcBasic_Clone.Models.FormContext;
+
+namespace MvcBasic_Clone.Controllers
+{
+    public class FormController : Controller
+    {
+        private FormContext db = new FormContext();
+        private FriendContext frienddb = new FriendContext();
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View(db.Infoes.ToList());
+        }
+
+        public ActionResult Indexfriend()
+        {
+            return View(frienddb.Employees.ToList());
+        }
+        // GET: Friends/Create
+        public ActionResult UserInfo()
+        {
+            return View();
+        }
+
+
+        // POST: Friends/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UserInfo([Bind(Include = "Id,Name,Phone,Email,Gender")] Info input)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Infoes.Add(input);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(input);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UserData(string name,string phone,string email,string gender)
+        {
+            Info info = new Info { Name = name, Phone = phone, Email = email, Gender = gender };
+
+            db.Infoes.Add(info);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcBasic_Clone/Models/Info.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcBasic_Clone.Models
+{
+    public class Info
+    {
+
+        [Display(Name = "編號")]
+        public int Id { get; set; }
+        [Display(Name = "姓名")]
+        [Required(ErrorMessage ="姓名必須輸入")]
+        [StringLength(20,MinimumLength =2)]
+        public string Name { get; set; }
+        [Display(Name = "電話")]
+        [Required(ErrorMessage = "電話必須輸入")]
+        [RegularExpression(@"^\d{4}\-?\d{3}\-?\d{3}$", ErrorMessage = "需為09xx-xxx-xxx格式")]
+        public string Phone { get; set; }
+        [Display(Name = "電子郵件")]
+        [Required(ErrorMessage = "電子郵件必須輸入")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Display(Name = "性別")]
+        [Required(ErrorMessage = "性別必須輸入")]
+        public string Gender { get; set; }
+    }
+}
